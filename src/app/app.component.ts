fa258149811baf392b281a138ee3db532720a257
Oscardev <121882462+oscardeveloper2000@users.noreply.github.com>
2023-01-25 16:08:10 -0500

    Punto de control, Fundamento de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,89 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-fundamentos';
+  // Atributos de clase
+  name :string | number | boolean= 'David';
+  bandera : boolean = !true;
+  widthImg : number=10;
+  newName !: string;
+  total: number = 25000;
+  today: Date = new Date();
+  otherDate: Date = new Date(2022, 1, 25)
+
+  person = {
+    name : 'Liliana',
+    age : 25,
+    avatar: 'https://www.informador.mx/__export/1591209620028/sites/elinformador/img/2020/06/03/whatsapp_image_2020-06-03_at_1_22_36_pm_x1x_crop1591209586178.jpg_788543494.jpg'
+  }
+
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  };
+
+
+  // Array
+  names: string[] = ['Valentina', 'Mike', 'Llilina', 'Andres'];
+
+ //Array de Objs
+ products: Product[] = [
+  {
+    name: 'EL mejor juguete',
+    price: 565,
+    image: './assets/images/toy.jpg',
+    category: 'all',
+
+  },
+  {
+    name: 'Bicicleta casi nueva',
+    price: 356,
+    image: './assets/images/bike.jpg'
+  },
+  {
+    name: 'Colleción de albumnes',
+    price: 34,
+    image: './assets/images/album.jpg'
+  },
+  {
+    name: 'Mis libros',
+    price: 23,
+    image: './assets/images/books.jpg'
+  },
+  {
+    name: 'Casa para perro',
+    price: 34,
+    image: './assets/images/house.jpg'
+  },
+  {
+    name: 'Gafas',
+    price: 3434,
+    image: './assets/images/glasses.jpg'
+  }
+]
+
+  constructor(){
+    // this.name=12;
+  }
+
+  // Metodos
+  increaseAge(){
+    this.person.age += 1;
+  }
+
+  onScroll(event : Event) {
+    const evento = event.target as HTMLElement;
+    console.log('Posición de Scrol: :>> ', evento.scrollTop);
+  }
+
+  addName(){
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number){
+    this.names.splice(index,1);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReversePipe,
+    TimeAgoPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
